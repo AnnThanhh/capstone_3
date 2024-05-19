8c329c39b62af484e33ebeadb3594001694e738a
AnnThanhh <trinhanthanh@gmail.com>
2024-05-19 10:58:27 +0700

    cm

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,18 @@
 import { Button } from "antd";
 import { NavLink, useNavigate } from "react-router-dom";
+import { logOut } from "../../redux/slices/user.slice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 
 export default function Header() {
+  const { currentUser } = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    dispatch(logOut());
+  };
   return (
     <div className="flex h-[92px] items-center justify-between px-4 container mx-auto">
       <NavLink
@@ -37,18 +47,27 @@ export default function Header() {
           Tin tức
         </NavLink>
       </div>
-      <div className="flex items-center gap-3">
-        <Button size="large" onClick={() => navigate("/auth/register")}>
-          Đăng ký
-        </Button>
-        <Button
-          size="large"
-          type="primary"
-          onClick={() => navigate("/auth/login")}
-        >
-          Đăng nhập
-        </Button>
-      </div>
+      {currentUser ? (
+        <>
+          <span>{currentUser.taiKhoan}</span>
+          <Button onClick={handleLogout} type="primary">
+            Đăng xuất
+          </Button>
+        </>
+      ) : (
+        <div className="flex items-center gap-3">
+          <Button size="large" onClick={() => navigate("/auth/register")}>
+            Đăng ký
+          </Button>
+          <Button
+            size="large"
+            type="primary"
+            onClick={() => navigate("/auth/login")}
+          >
+            Đăng nhập
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
